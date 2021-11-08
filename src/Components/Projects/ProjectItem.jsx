f88e80bd48527f30c9e5dd67f88af3b4bde5d322
AntonioSkopin <askopin@outlook.com>
2021-11-08 22:10:38 +0100

    #21 made project images responsive

--- src/Components/Projects/ProjectItem.jsx
+++ src/Components/Projects/ProjectItem.jsx
@@ -8,7 +8,7 @@ const ProjectItem = props => {
             <div class="menu__item py-4">
                 <a href={props.link} class="menu__item-link font-bold text-7xl md:text-9xl">{props.projectTitle}</a>
                 <img class="menu__item-img" src={props.projectIMG} alt="Project image"/>
-                <div class="marquee font-bold text-7xl md:text-9xl">
+                <div class="marquee font-bold text-7xl md:text-9xl x">
                     <div class="marquee__inner" aria-hidden="true">
                         <span>{props.projectTitle}</span>
                         <span>{props.projectTitle}</span>
